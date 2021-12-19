1c622bd14980aeabd92181f46cb25188fc46dc5d
ktelagar <ktelagar@gmail.com>
2021-12-20 01:14:04 +0530

    Update changes

---
+++ DemoProj1/src/com/citi/dao/Darjeeling.java
@@ -0,0 +1,19 @@
+package com.citi.dao;
+
+public class Darjeeling {
+	
+	public static String DarjeelingMoments()
+	{
+		String item;
+		
+		item = "Pink Sweater";
+		
+		return item;
+	}
+
+	public static void main(String args[])
+	{
+		DarjeelingMoments();
+			
+	}
+}
